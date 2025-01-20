c602b856cda8bd1df37804fde920a887ec4b6d4a
edupoly <info.edupoly@gmail.com>
2025-01-20 10:06:26 +0530

    routingIssueCleared

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -3,19 +3,20 @@ import { useLocation, useParams } from 'react-router-dom'
 
 function CountryDetails() {
     var x = useParams()
-    var [details,setDetails] = useState({})
+    var [details,setDetails] = useState(null)
     useEffect(()=>{
         fetch(`https://restcountries.com/v3/name/${x.cname}`)
         .then(res=>res.json())
         .then(data=>{
+          console.log(data)
             setDetails({...data[0]})
         })
     },[x.cname])
   return (
     <div className='container'>
-        <h1>{x.cname} CountryDetails</h1>
-        <h2>{details.population}</h2>
-        <img src={details.flags[0]} className='w-100' alt="" />
+        <h1>{x?.cname} CountryDetails</h1>
+        <h2>{details?.population}</h2>
+        <img src={details?.flags[0]} className='w-100' alt="" />
     </div>
   )
 }
